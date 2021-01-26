5f2e4599f2415b1fb14d16e2d71ffe281dc052f1
Andreas <propiece02@gmail.com>
2021-01-26 12:35:26 +0100

    commit

---
+++ Grafik/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Raylib_cs;
+
+namespace Grafik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+         Raylib.InitWindow(800, 600, "Hello Te");
+
+         Color myColor = new Color(0, 255, 128, 255);
+
+         float x = 0;
+         float y = 0;
+         
+        while(!Raylib.WindowShouldClose())
+        {
+
+            if(Raylib.IsKeyDown(KeyboardKey.KEY_RIGHT))
+            {
+                x += .1f;
+            }
+            if(Raylib.IsKeyDown(KeyboardKey.KEY_LEFT))
+            {
+                 x -= .1f;
+            }
+            if(Raylib.IsKeyDown(KeyboardKey.KEY_DOWN))
+            {
+               y += .1f;
+            }
+            if(Raylib.IsKeyDown(KeyboardKey.KEY_UP))
+            {
+               y -= .1f;
+            }
+           
+         
+         Raylib.BeginDrawing();
+
+         Raylib.ClearBackground(myColor);
+
+         Raylib.DrawRectangle((int)x,(int)y, 20, 20, Color.PINK);
+
+         Console.WriteLine();
+
+         Raylib.EndDrawing();
+
+        }
+
+
+
+
+        }
+    }
+}
